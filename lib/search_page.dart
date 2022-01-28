baeedae6175470964c1863dede904dd384e02f79
elisha_b <elisha.b@cestasoft.co.za>
2022-01-28 16:46:41 +0200

    fix firebase errors and change mnSdk to 21

--- lib/search_page.dart
+++ lib/search_page.dart
@@ -6,15 +6,30 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter_signin_button/button_list.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'product_card_widget.dart';
 // import 'search_card.dart';
 
 // FirebaseFirestore firestore = FirebaseFirestore.instance;
 
 // Search Page
-class GlobalSearchScreen extends StatelessWidget {
+class GlobalSearchScreen extends StatefulWidget {
   static const routeName = '/search';
   const GlobalSearchScreen({Key? key}) : super(key: key);
+
+  @override
+  State<GlobalSearchScreen> createState() => _GlobalSearchScreenState();
+}
+
+class _GlobalSearchScreenState extends State<GlobalSearchScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Firebase.initializeApp().whenComplete(() {
+      print("completed");
+      getProductList();
+    });
+  }
 
   Future<bool> _goback(BuildContext context) {
     return Navigator.of(context)
@@ -26,191 +41,57 @@ class GlobalSearchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            leading: IconButton(
-              icon: const Icon(FontAwesomeIcons.arrowLeft),
-              onPressed: () => _goback(context),
-            ),
-            // The search area here
-            title: Container(
-              width: double.infinity,
-              height: 40,
-              decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(5)),
-              child: Center(
-                child: TextField(
-                  decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.search),
-                      suffixIcon: IconButton(
-                        icon: Icon(Icons.clear),
-                        onPressed: () {
-                          /* Clear the search field */
-                        },
-                      ),
-                      hintText: 'Search...',
-                      border: InputBorder.none),
-                ),
+      appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(FontAwesomeIcons.arrowLeft),
+            onPressed: () => _goback(context),
+          ),
+          // The search area here
+          title: Container(
+            width: double.infinity,
+            height: 40,
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(5)),
+            child: Center(
+              child: TextField(
+                decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.search),
+                    suffixIcon: IconButton(
+                      icon: Icon(Icons.clear),
+                      onPressed: () {
+                        /* Clear the search field */
+                      },
+                    ),
+                    hintText: 'Search...',
+                    border: InputBorder.none),
               ),
-            )),
-        body: GridView.count(
-          addAutomaticKeepAlives: false,
-          cacheExtent: 100.00,
-          shrinkWrap: true,
-          crossAxisCount: 2,
-          children: [
-            ProductCardWidget(),
-            ProductCardWidget(),
-            ProductCardWidget()
-            // constructing cards
-          ],
-        )
-        // Column(
-        //   mainAxisAlignment: MainAxisAlignment.start,
-        //   crossAxisAlignment: CrossAxisAlignment.start,
+            ),
+          )),
+      body: GridView.count(
+        addAutomaticKeepAlives: false,
+        cacheExtent: 100.00,
+        shrinkWrap: true,
+        crossAxisCount: 2,
+        children: [
+          ProductCardWidget(),
+          ProductCardWidget(),
+          ProductCardWidget()
+          // constructing cards
+        ],
+      ),
+    );
+  }
 
-        //   children: [
-        //   Expanded(child: Container(
-        //     flex
-        //   )),
-        //     Column(
-        //       mainAxisAlignment: MainAxisAlignment.start,
-        //       crossAxisAlignment: CrossAxisAlignment.start,
-        //       children: [
-        //         SizedBox(
-        //           width: 100.00,
-        //           height: 100.00,
-        //           child: Container(
-        //             alignment: Alignment.topLeft,
-        //             padding: const EdgeInsets.fromLTRB(0.00, 5.00, 0.00, 0.00),
-        //             // constraints: BoxConstraints.expand(height: 50.00, width: 50.00),
-        //             width: 100.00,
-        //             child: Image.asset(
-        //               'assets/images/pnp.jpg',
-        //               fit: BoxFit.fitWidth,
-        //               height: 100.00,
-        //               width: 100.00,
-        //             ),
-
-        //             // Container(child: ImageContainer("asserts/images/ok.png")),
-        //           ),
-        //         ),
-        //         Container(
-        //           alignment: Alignment.topLeft,
-        //           child: Text('Name',
-        //               textAlign: TextAlign.start,
-        //               style: TextStyle(
-        //                 fontSize: 15.0,
-        //               )),
-        //         ),
-        //         Container(
-        //           alignment: Alignment.topLeft,
-        //           child: Text(
-        //             'Description Description is that',
-        //             style: TextStyle(fontSize: 10.00),
-        //           ),
-        //         ),
-        //         Container(
-        //           alignment: Alignment.bottomLeft,
-        //           child: Row(
-        //             children: [
-        //               Text(
-        //                 '\$',
-        //                 style: TextStyle(fontSize: 15.00),
-        //               ),
-        //               Text(
-        //                 'Price',
-        //                 style: TextStyle(fontSize: 15.00),
-        //               )
-        //             ],
-        //           ),
-        //         ),
-        //         Container(
-        //             alignment: Alignment.bottomLeft,
-        //             child: TextButton.icon(
-        //               style: TextButton.styleFrom(
-        //                   textStyle: TextStyle(color: Colors.white),
-        //                   backgroundColor: Colors.grey,
-        //                   shape: RoundedRectangleBorder(
-        //                       borderRadius: BorderRadius.circular(10.00))),
-        //               onPressed: () => {},
-        //               label: Text(
-        //                 'Add to cart',
-        //                 style: TextStyle(fontSize: 10.0),
-        //               ),
-        //               icon: Icon(Icons.shopping_bag_outlined),
-        //             ))
-        //       ],
-        //     ),
-        //     Column(
-        //       mainAxisAlignment: MainAxisAlignment.start,
-        //       crossAxisAlignment: CrossAxisAlignment.start,
-        //       children: [
-        //         SizedBox(
-        //           width: 100.00,
-        //           height: 100.00,
-        //           child: Container(
-        //             alignment: Alignment.topLeft,
-        //             padding: const EdgeInsets.fromLTRB(0.00, 5.00, 0.00, 0.00),
-        //             // constraints: BoxConstraints.expand(height: 50.00, width: 50.00),
-        //             width: 100.00,
-        //             child: Image.asset(
-        //               'assets/images/pnp.jpg',
-        //               fit: BoxFit.fitWidth,
-        //               height: 100.00,
-        //               width: 100.00,
-        //             ),
-
-        //             // Container(child: ImageContainer("asserts/images/ok.png")),
-        //           ),
-        //         ),
-        //         Container(
-        //           alignment: Alignment.topLeft,
-        //           child: Text('Name',
-        //               textAlign: TextAlign.start,
-        //               style: TextStyle(
-        //                 fontSize: 15.0,
-        //               )),
-        //         ),
-        //         Container(
-        //           alignment: Alignment.topLeft,
-        //           child: Text(
-        //             'Description Description is that',
-        //             style: TextStyle(fontSize: 10.00),
-        //           ),
-        //         ),
-        //         Container(
-        //           alignment: Alignment.bottomLeft,
-        //           child: Row(
-        //             children: [
-        //               Text(
-        //                 '\$',
-        //                 style: TextStyle(fontSize: 15.00),
-        //               ),
-        //               Text(
-        //                 'Price',
-        //                 style: TextStyle(fontSize: 15.00),
-        //               )
-        //             ],
-        //           ),
-        //         ),
-        //         Container(
-        //             alignment: Alignment.bottomLeft,
-        //             child: TextButton.icon(
-        //               style: TextButton.styleFrom(
-        //                   textStyle: TextStyle(color: Colors.white),
-        //                   backgroundColor: Colors.grey,
-        //                   shape: RoundedRectangleBorder(
-        //                       borderRadius: BorderRadius.circular(10.00))),
-        //               onPressed: () => {},
-        //               label: Text(
-        //                 'Add to cart',
-        //                 style: TextStyle(fontSize: 10.0),
-        //               ),
-        //               icon: Icon(Icons.shopping_bag_outlined),
-        //             ))
-        //       ],
-        //     )
-        //   ],
-        );
+  getProductList() async {
+    var collection = FirebaseFirestore.instance.collection('basics');
+    var querySnapshot = await collection.get();
+    print(querySnapshot);
+    print("results");
+    for (var queryDocumentSnapshot in querySnapshot.docs) {
+      Map<String, dynamic> data = queryDocumentSnapshot.data();
+      var name = data['Name'];
+      // var phone = data['phone'];
+      print(name);
+    }
   }
 }
